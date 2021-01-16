a7b6a042a10985e204e911fb3d40f9290526e668
Vyacheslav Aprosinckin <vyacheslav.aprosinckin@yandex.ru>
2021-01-16 17:39:01 +0300

    Programm commission percent added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main() {
+    val amount = 10000.0
+    val commissionPercent = 0.75
+    val minCommission = 35
+    var commission = (amount * commissionPercent) / 100
+    if (commission < minCommission) {
+        commission = minCommission.toDouble()
+    }
+    val totalAmount = amount - commission
 
+    println(
+        "Перевод на сумму: $amount руб.\n" +
+                "Комиссия: $commissionPercent" + '%' + " (минимум $minCommission рублей): " + "$commission руб.\n" +
+                "Сумма к переводу: $totalAmount руб."
+    )
 }
